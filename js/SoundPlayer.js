67ca5cb9ad1eb0a9abc3f62d2becad5986f1f555
Taisuke Kawamata <j4417701@gmail.com>
2020-04-06 21:31:53 +0900

    Responsive

---
+++ js/SoundPlayer.js
@@ -0,0 +1,129 @@
+'use strict';
+
+class SoundPlayer {
+
+    audioElement = new Audio();
+
+    /**
+     * 
+     * @param {str} url 
+     */
+    constructor(url) {
+        this.audioElement.src = url;
+        
+        /* 音声データのメタデータロード完了時に起動 */
+        this.audioElement.addEventListener("loadedmetadata", () => {
+            this.init(); // 初期化
+        });
+    }
+
+    /**
+     * 初期化
+     */
+    init() {
+
+        this.PlaySoundInitializer();
+        this.SeekbarInitializer();
+
+
+        const endTimeDisplayer = document.getElementById("endTimeDisplayer");
+        const videoDuration = this.audioElement.duration;
+        endTimeDisplayer.innerHTML = Timecode2HMS(Math.floor(videoDuration)).slice(3);
+
+
+        const currentTimeDisplayer = document.getElementById("currentTimeDisplayer");
+        currentTimeDisplayer.innerHTML = Timecode2HMS(0).slice(3);
+
+        this.audioElement.ontimeupdate = () => {
+            const currentTime = this.audioElement.currentTime;
+            const videoTimeHMS = Timecode2HMS(Math.floor(currentTime)).slice(3);
+            const currentTimeDisplayer = document.getElementById("currentTimeDisplayer");
+
+            document.getElementById("videoSeekbar").value = currentTime; // CreateSeekbarで循環しそう
+            currentTimeDisplayer.innerHTML = videoTimeHMS;
+            currentTimeDisplayer.value = currentTime;
+            // if (document.hasFocus())
+            //     document.title = "VideoNote";
+            // else // 非フォーカス時はタイトルを動画情報に変更
+            //     document.title = _videoObjectList[_nowVideoIndex].name.slice(0, 4) + "[" + videoTimeHMS + "]";
+        };
+
+
+
+        const soundbar = document.getElementById("soundController");
+        soundbar.oninput = () => {
+            this.audioElement.volume = document.getElementById("soundController").value;
+        };
+    }
+
+    PlaySoundInitializer() {
+        document.getElementById("videoPlay").onpointerdown = () => {
+            soundObject.ClickPlayButton();
+        }
+    }
+
+    SeekbarInitializer(){
+        const seekbar = document.getElementById("videoSeekbar");
+        seekbar.max = this.audioElement.duration;
+
+        seekbar.oninput = () => {
+            const seekbarValue = document.getElementById("videoSeekbar").value;
+            this.audioElement.currentTime = seekbarValue;
+            document.getElementById("currentTimeDisplayer").innerHTML = Timecode2HMS(Math.floor(seekbarValue)).slice(3);
+        };
+
+    }
+
+    PlaySound() {
+        console.log("play", this.audioElement);
+        this.audioElement.play();
+    }
+    StopSound() {
+        console.log("stop", this.audioElement);
+        this.audioElement.pause();
+    }
+    ClickPlayButton() {
+        const icon = document.getElementById("videoPlayIcon");
+        if (this.audioElement.paused) {
+            this.audioElement.play();
+            if (icon.classList.contains("fa-play"))
+                icon.classList.remove("fa-play");
+            icon.classList.add("fa-pause");
+        } else {
+            this.audioElement.pause();
+            if (icon.classList.contains("fa-pause"))
+                icon.classList.remove("fa-pause");
+            icon.classList.add("fa-play");
+        }
+    }
+    ChangeSoundVolume() {
+        this.audioElement.volume = document.getElementById("sound-controller").value;
+    }
+}
+
+/**
+ * 秒を時分秒に修正
+ * @param {String} 時分秒の文字列 
+ */
+function Timecode2HMS(timeCode) {
+    let hms = "";
+    const h = timeCode / 3600 | 0;
+    const m = timeCode % 3600 / 60 | 0;
+    const s = parseInt(timeCode) % 60;
+
+    if (h != 0)
+        hms = ZeroPadding(h) + "：" + ZeroPadding(m) + "：" + ZeroPadding(s);
+    else if (m != 0)
+        hms = "00：" + ZeroPadding(m) + "：" + ZeroPadding(s);
+    else
+        hms = "00：00：" + ZeroPadding(s);
+    return hms;
+}
+/**
+* 映像時間の0埋め
+* @param {String} timeCode 
+*/
+function ZeroPadding(timeCode) {
+    if (timeCode < 10) return "0" + timeCode;
+    else return timeCode;
+}
